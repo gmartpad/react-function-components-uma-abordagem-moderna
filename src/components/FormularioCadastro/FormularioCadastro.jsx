91a7539740701d8ef9841b8565e1a3866d7195bc
gmartpad <gabrielmpwork@gmail.com>
2021-02-18 23:49:28 -0300

    feat: facilitando a prototipacao

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import Button from "@material-ui/core/Button";
 
 function FormularioCadastro(){
 
     return(
         <form>
-            <input type="text"/>
+
+            <label for="nome">Nome</label>
+            <input id="nome" type="text"/>
+
+            <label for="sobrenome">Sobrenome</label>
+            <input id="sobrenome" type="text"/>
+
+            <label for="cpf">CPF</label>
+            <input id="cpf" type="text"/>
+
+            <label for="promocoes">Promoções</label>
+            <input id="promocoes" type="checkbox"/>
+
+            <label for="novidades">Novidades</label>
+            <input id="novidades" type="checkbox"/>
+
+            <Button 
+                type="submit"
+                variant="contained"
+                color="primary"
+            >
+                Cadastrar
+            </Button>
+
         </form>
     );
 
